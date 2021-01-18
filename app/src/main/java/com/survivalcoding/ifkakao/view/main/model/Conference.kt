c1796c0e8b8ad805247e01e8a1326cb7fca9c459
qufwnfahs <style_55@naver.com>
2021-01-18 23:40:16 +0900

    변수명과 동일한 Json Annotation 제거

--- app/src/main/java/com/survivalcoding/ifkakao/view/main/model/Conference.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/view/main/model/Conference.kt
@@ -7,15 +7,10 @@ import com.squareup.moshi.JsonClass
 @kotlinx.android.parcel.Parcelize
 @JsonClass(generateAdapter = true)
 data class Conference(
-    @Json(name = "name")
     val name: String,
-    @Json(name = "link")
     val link: String,
-    @Json(name = "start")
     val start: String,
-    @Json(name = "end")
     val end: String,
-    @Json(name = "location")
     val location: String,
     @Json(name = "cocoa-only")
     val cocoaOnly: Boolean? = null,
