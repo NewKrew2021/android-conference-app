38debc1f50eeacff7068e7fac4598731b0045625
qufwnfahs <style_55@naver.com>
2021-01-18 18:22:58 +0900

    Fragment 초기화 xml name attribute 이용

--- app/src/main/java/com/survivalcoding/ifkakao/view/main/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/view/main/MainActivity.kt
@@ -2,11 +2,7 @@ package com.survivalcoding.ifkakao.view.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import com.survivalcoding.ifkakao.R
 import com.survivalcoding.ifkakao.databinding.ActivityMainBinding
-import com.survivalcoding.ifkakao.view.main.fragment.ConferenceListFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -15,12 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<ConferenceListFragment>(R.id.fragment_container_view)
-            }
-        }
     }
 }
