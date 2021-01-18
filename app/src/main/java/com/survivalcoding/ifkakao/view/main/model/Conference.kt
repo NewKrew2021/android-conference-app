e204a88bc9693124a355167d0f05af376f76b7eb
qufwnfahs <style_55@naver.com>
2021-01-18 19:31:47 +0900

    Parcelize 적용

--- app/src/main/java/com/survivalcoding/ifkakao/view/main/model/Conference.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/view/main/model/Conference.kt
@@ -1,8 +1,10 @@
 package com.survivalcoding.ifkakao.view.main.model
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+@kotlinx.android.parcel.Parcelize
 @JsonClass(generateAdapter = true)
 data class Conference(
     @Json(name = "name")
@@ -17,4 +19,4 @@ data class Conference(
     val location: String,
     @Json(name = "cocoa-only")
     val cocoaOnly: Boolean? = null,
-)
+) : Parcelable
