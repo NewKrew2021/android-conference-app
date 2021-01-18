afee512968727e01154124a72708dd569d269fd2
tony.mb <tony.mb@kakaocorp.com>
2021-01-18 18:56:07 +0900

    TODO 추가

--- app/src/main/java/com/survivalcoding/ifkakao/model/ConferenceRepository.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/model/ConferenceRepository.kt
@@ -3,8 +3,10 @@ package com.survivalcoding.ifkakao.model
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 
+// TODO: Retrofit2 / OkHttp3 적용해서 더미 데이터 대체
 class ConferenceRepository : Repository {
     override fun getConferenceData(): List<ConferenceInfo> {
+
         val moshi = Moshi.Builder().build()
         val listType = Types.newParameterizedType(List::class.java, ConferenceInfo::class.java)
         val adapter = moshi.adapter<List<ConferenceInfo>>(listType)
