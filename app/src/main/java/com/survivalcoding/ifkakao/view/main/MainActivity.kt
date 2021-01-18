51f1d195a82d492f7f8ac97cb1e4ef93aa96620c
qufwnfahs <style_55@naver.com>
2021-01-18 18:13:08 +0900

    ViewBinding 적용

--- app/src/main/java/com/survivalcoding/ifkakao/view/main/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/view/main/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.survivalcoding.ifkakao.R
+import com.survivalcoding.ifkakao.databinding.ActivityMainBinding
 import com.survivalcoding.ifkakao.view.main.fragment.ConferenceListFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
--- app/src/main/java/com/survivalcoding/ifkakao/view/main/fragment/ConferenceDetailFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/view/main/fragment/ConferenceDetailFragment.kt
@@ -1,5 +1,28 @@
 package com.survivalcoding.ifkakao.view.main.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.survivalcoding.ifkakao.databinding.FragmentConferenceDetailBinding
 
-class ConferenceDetailFragment : Fragment()
+class ConferenceDetailFragment : Fragment() {
+    private var _binding: FragmentConferenceDetailBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentConferenceDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
