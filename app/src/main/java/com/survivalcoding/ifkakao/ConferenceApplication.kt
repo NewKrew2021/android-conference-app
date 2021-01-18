76c164447a2cacabfd47bb7d27bd99fa82e957c6
finn k <finn.k@kakaocorp.com>
2021-01-18 18:11:48 +0900

    - BaseCode 작성 - Application 설정

---
+++ app/src/main/java/com/survivalcoding/ifkakao/ConferenceApplication.kt
@@ -0,0 +1,14 @@
+package com.survivalcoding.ifkakao
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class ConferenceApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@ConferenceApplication)
+        }
+    }
+}
