8116a500823b0ce36f5d9ee415a189a13d20fd70
dale.band <dale.band@kakaocorp.com>
2021-01-18 17:00:01 +0900

    activity파일(UI) 생성

---
+++ app/src/main/java/com/survivalcoding/ifkakao/conference/view/ConferenceActivity.kt
@@ -0,0 +1,13 @@
+package com.survivalcoding.ifkakao.conference.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.survivalcoding.ifkakao.R
+
+class ConferenceActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conference)
+
+    }
+}
