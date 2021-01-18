40ffc237542a76221cd940636f2c29384419bade
finn k <finn.k@kakaocorp.com>
2021-01-18 15:40:18 +0900

    - package setting - gradle setting

--- app/src/main/java/com/survivalcoding/ifkakao/ui/view/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.survivalcoding.ifkakao
+package com.survivalcoding.ifkakao.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.survivalcoding.ifkakao.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
