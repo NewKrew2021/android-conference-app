1e918c306aabeb4714ce076829aad3f6e49fe808
qufwnfahs <style_55@naver.com>
2021-01-19 15:07:44 +0900

    toolbar에 menu 연결

--- ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/MainActivity.kt
+++ ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.jayden.ifkakaoclone
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.jayden.ifkakaoclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
     }
 }
