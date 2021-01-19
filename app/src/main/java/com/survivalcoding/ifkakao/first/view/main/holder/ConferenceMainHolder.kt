9549d3a6acd1d65ce15abc7a9fb28396e836b295
summer.dev <summer.dev@kakaocorp.com>
2021-01-19 17:28:28 +0900

    - RecyclerView의 ViewHolder적용

---
+++ app/src/main/java/com/survivalcoding/ifkakao/first/view/main/holder/ConferenceMainHolder.kt
@@ -0,0 +1,6 @@
+package com.survivalcoding.ifkakao.first.view.main.holder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.survivalcoding.ifkakao.databinding.ItemConferenceBinding
+
+class ConferenceMainHolder(binding: ItemConferenceBinding) : RecyclerView.ViewHolder(binding.root)
