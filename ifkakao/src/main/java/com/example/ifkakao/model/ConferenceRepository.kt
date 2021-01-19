62b685fee347e158a1073937741e78c9eead81c1
tony.mb <tony.mb@kakaocorp.com>
2021-01-19 17:52:05 +0900

    Repository 클래스 구현

--- ifkakao/src/main/java/com/example/ifkakao/model/ConferenceRepository.kt
+++ ifkakao/src/main/java/com/example/ifkakao/model/ConferenceRepository.kt
@@ -1,4 +1,14 @@
 package com.example.ifkakao.model
 
-class ConferenceRepository {
+import com.example.ifkakao.model.jsonformat.ConferenceData
+import com.squareup.moshi.Moshi
+
+// 더미데이터가 너무 커서 txt 파일로 저장했는데, Repository 에서 txt 파일을 사용하려면 context 가 필요함.
+// 네트워크 연동하기 전까지 Repository 의 생성자로 더미데이터를 주입시켜줘야할 것 같음.
+class ConferenceRepository(private val dummyData: String) : Repository {
+    override fun getConferenceData(): ConferenceData {
+        val moshi = Moshi.Builder().build()
+        val adapter = moshi.adapter(ConferenceData::class.java)
+        return adapter.fromJson(dummyData) ?: ConferenceData(false, 0, listOf(), null)
+    }
 }
--- ifkakao/src/main/java/com/example/ifkakao/model/Repository.kt
+++ ifkakao/src/main/java/com/example/ifkakao/model/Repository.kt
@@ -1,4 +1,7 @@
 package com.example.ifkakao.model
 
+import com.example.ifkakao.model.jsonformat.ConferenceData
+
 interface Repository {
+    fun getConferenceData(): ConferenceData
 }
