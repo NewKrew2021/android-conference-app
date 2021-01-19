4ddd18a741940d1ab6a954ec0219d34ed6156ffe
summer.dev <summer.dev@kakaocorp.com>
2021-01-19 17:01:52 +0900

    - fragment에 viewbinding 적용

--- app/src/main/java/com/survivalcoding/ifkakao/first/view/ListFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/first/view/ListFragment.kt
@@ -1,8 +1,31 @@
 package com.survivalcoding.ifkakao.first.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.survivalcoding.ifkakao.R
+import com.survivalcoding.ifkakao.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment(R.layout.fragment_list) {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        requireActivity().title = "Conference List"
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
