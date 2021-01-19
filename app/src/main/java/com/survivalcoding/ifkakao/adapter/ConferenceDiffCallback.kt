d4344a0b30ca2ff1da6b07cb8a800d668d4a94da
bsgreentea1 <bsgreentea1@gmail.com>
2021-01-19 19:03:11 +0900

    불필요 코드 제거

--- app/src/main/java/com/survivalcoding/ifkakao/adapter/ConferenceDiffCallback.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/adapter/ConferenceDiffCallback.kt
@@ -2,7 +2,6 @@ package com.survivalcoding.ifkakao.adapter
 
 import androidx.recyclerview.widget.DiffUtil
 import com.survivalcoding.ifkakao.model.Conference
-import com.survivalcoding.ifkakao.model.SampleItem
 
 object ConferenceDiffCallback : DiffUtil.ItemCallback<Conference>() {
     override fun areItemsTheSame(oldItem: Conference, newItem: Conference): Boolean {
@@ -12,5 +11,4 @@ object ConferenceDiffCallback : DiffUtil.ItemCallback<Conference>() {
     override fun areContentsTheSame(oldItem: Conference, newItem: Conference): Boolean {
         return oldItem == newItem
     }
-
 }
