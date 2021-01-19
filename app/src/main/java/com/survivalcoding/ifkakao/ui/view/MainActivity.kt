7a37f970785447c6bf23fb2ee0b7dd7be6b7ae86
finn k <finn.k@kakaocorp.com>
2021-01-19 14:49:28 +0900

    - Main toolbar menu 생성

--- app/src/main/java/com/survivalcoding/ifkakao/ui/view/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/ui/view/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.survivalcoding.ifkakao.ui.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.survivalcoding.ifkakao.R
 
@@ -9,5 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar_main))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.toolbar_item_main -> {
+                Toast.makeText(applicationContext, "menu click!", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
