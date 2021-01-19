6c96325b701b469fc539dc457f952649b36b1afc
Sanghun-Lee <paa0609@naver.com>
2021-01-19 17:43:42 +0900

    상세화면 제작, bundle로 넘겨준 데이터 받기

--- app/src/main/java/com/survivalcoding/ifkakao/conference/view/conference/ConferenceFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/conference/view/conference/ConferenceFragment.kt
@@ -1,16 +1,48 @@
 package com.survivalcoding.ifkakao.conference.view.conference
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.survivalcoding.ifkakao.R
 import com.survivalcoding.ifkakao.conference.data.DataModelItem
+import com.survivalcoding.ifkakao.databinding.FragmentConferenceBinding
 
 class ConferenceFragment(private val data: List<DataModelItem>) :
     Fragment(R.layout.fragment_conference) {
 
+    private var _binding: FragmentConferenceBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentConferenceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val index = Bundle().getInt("item")
+        val index = requireArguments().getInt("index")
+        val item = data[index]
+
+        requireActivity().title = item.name
+        Log.d("TAG", "onBindViewHolder: item : ${item}, index : ${index}")
+
+        binding.apply {
+            location.text = item.location
+            conferenceDate.text = "${item.start} ~ ${item.end}"
+            conferenceWebsite.text = item.link
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
