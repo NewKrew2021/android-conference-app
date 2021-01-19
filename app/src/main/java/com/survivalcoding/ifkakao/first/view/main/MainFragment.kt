d8b10e182c7074cb12f514083e042c8aceba923f
summer.dev <summer.dev@kakaocorp.com>
2021-01-19 17:38:19 +0900

    - RecyclerView 연동

--- app/src/main/java/com/survivalcoding/ifkakao/first/view/main/MainFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/first/view/main/MainFragment.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.survivalcoding.ifkakao.databinding.FragmentMainBinding
 import com.survivalcoding.ifkakao.first.model.repository.Repository
+import com.survivalcoding.ifkakao.first.view.main.adapter.ConferenceMainAdapter
 
 
 class MainFragment(private val repository: Repository) : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private val adapter = ConferenceMainAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +26,18 @@ class MainFragment(private val repository: Repository) : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.adapter = adapter
+        updateUI()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateUI() {
+        adapter.submitList(repository.getData())
     }
 }
