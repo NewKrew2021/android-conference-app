49907d9e1b62a9f8b3caf7b62310ff1ebe0f3982
qufwnfahs <style_55@naver.com>
2021-01-19 19:17:24 +0900

    title HTML tag 제거 및 bind() 로직 수정

--- ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/view/main/holder/SessionViewHolder.kt
+++ ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/view/main/holder/SessionViewHolder.kt
@@ -1,5 +1,6 @@
 package com.jayden.ifkakaoclone.view.main.holder
 
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.jayden.ifkakaoclone.R
@@ -12,16 +13,15 @@ class SessionViewHolder(private val binding: SessionItemLayoutBinding) :
     fun bind(item: Session) {
         with(binding) {
             textField.text = item.field ?: ""
-            textTitle.text = item.title ?: ""
+            textTitle.text = item.title?.takeIf { it.isNotEmpty() }?.let {
+                HtmlCompat.fromHtml(item.title, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+            } ?: ""
+            textVideoLength.text = item.linkList?.video?.takeIf { it.isNotEmpty() }?.get(0)?.description ?: ""
 
             item.linkList?.pcImage?.takeIf { it.isNotEmpty() }?.get(0)?.url?.let {
                 thumbnail.load(it) {
                     placeholder(R.drawable.image_placeholder)
                 }
-            }
-
-            item.linkList?.video?.takeIf { it.isNotEmpty() }?.get(0)?.description?.let {
-                textVideoLength.text = it
             }
         }
     }
