1e3ff1560e0b75004b680adcbb8a1bbe67a9183f
summer.dev <summer.dev@kakaocorp.com>
2021-01-19 18:03:36 +0900

    - data class를 parcelable data로 변환

--- app/src/main/java/com/survivalcoding/ifkakao/first/model/Conference.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/first/model/Conference.kt
@@ -1,8 +1,11 @@
 package com.survivalcoding.ifkakao.first.model
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 data class Conference(
         @Json(name = "cocoa-only")
@@ -12,4 +15,4 @@ data class Conference(
         val location: String,
         val name: String,
         val start: String
-)
+) : Parcelable
