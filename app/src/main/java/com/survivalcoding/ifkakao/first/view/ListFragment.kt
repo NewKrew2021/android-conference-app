525ac447210c7079f12fd711b1fd4cfdf51889f6
summer.dev <summer.dev@kakaocorp.com>
2021-01-19 16:58:45 +0900

    - fragment 생성

---
+++ app/src/main/java/com/survivalcoding/ifkakao/first/view/ListFragment.kt
@@ -0,0 +1,8 @@
+package com.survivalcoding.ifkakao.first.view
+
+import androidx.fragment.app.Fragment
+import com.survivalcoding.ifkakao.R
+
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+}
--- app/src/main/java/com/survivalcoding/ifkakao/first/view/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/first/view/MainActivity.kt
@@ -2,6 +2,9 @@ package com.survivalcoding.ifkakao.first.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.survivalcoding.ifkakao.R
 import com.survivalcoding.ifkakao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<ListFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
