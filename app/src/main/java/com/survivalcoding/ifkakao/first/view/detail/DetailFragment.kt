a940a8cd8169db75ed69782c03e22843262ceaa1
summer.dev <summer.dev@kakaocorp.com>
2021-01-19 18:31:36 +0900

    - hyperlink 구현

--- app/src/main/java/com/survivalcoding/ifkakao/first/view/detail/DetailFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/first/view/detail/DetailFragment.kt
@@ -1,13 +1,19 @@
 package com.survivalcoding.ifkakao.first.view.detail
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.URLSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.survivalcoding.ifkakao.R
 import com.survivalcoding.ifkakao.databinding.FragmentDetailBinding
 import com.survivalcoding.ifkakao.first.model.Conference
 import com.survivalcoding.ifkakao.first.view.MainActivity
+
 
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
@@ -25,9 +31,20 @@ class DetailFragment : Fragment() {
 
         arguments?.getParcelable<Conference>(MainActivity.MAIN_TO_DETAIL)?.let {
             item = it
+            val dateText = "${item.start} - ${item.end}"
             binding.nameText.text = item.name
             binding.locationText.text = item.location
-            binding.dateText.text = "${item.start} - ${item.end}"
+            binding.dateText.text = dateText
+            val linkText = SpannableString(getString(R.string.hyperlink_text)).apply {
+                setSpan(
+                    URLSpan(item.link),
+                    0,
+                    getString(R.string.hyperlink_text).length,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+            binding.linkText.movementMethod = LinkMovementMethod.getInstance()
+            binding.linkText.text = linkText
         }
 
         return binding.root
