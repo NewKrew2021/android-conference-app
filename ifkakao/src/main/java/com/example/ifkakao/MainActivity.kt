ba2fd98ae88755c23c8b4f286f539a34d62ec421
tony.mb <tony.mb@kakaocorp.com>
2021-01-20 13:29:57 +0900

    코드 정리

--- ifkakao/src/main/java/com/example/ifkakao/MainActivity.kt
+++ ifkakao/src/main/java/com/example/ifkakao/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.ifkakao
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ifkakao.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
