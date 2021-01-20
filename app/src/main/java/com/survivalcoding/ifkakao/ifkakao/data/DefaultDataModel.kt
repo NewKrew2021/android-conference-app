78508ae959a4009e0ee3c01226ae6dc0c5b8a20f
Sanghun-Lee <paa0609@naver.com>
2021-01-20 20:32:45 +0900

    Dummy데이터 추가

--- app/src/main/java/com/survivalcoding/ifkakao/ifkakao/data/DefaultDataModel.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/ifkakao/data/DefaultDataModel.kt
@@ -3,5 +3,7 @@ package com.survivalcoding.ifkakao.ifkakao.data
 import com.survivalcoding.ifkakao.ifkakao.model.IfKakaoItem
 
 interface DefaultDataModel {
-    fun getIfKakaoItem(json: String) : IfKakaoItem
+    fun getIfKakaoItem(json: String): IfKakaoItem
+
+    fun getRequest(): String
 }
