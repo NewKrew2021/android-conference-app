d1e49a26b5475c537004f866fa7364184a08abdb
bsgreentea1 <bsgreentea1@gmail.com>
2021-01-20 16:28:36 +0900

    모델 추가 - 이미지를 얻기 위해 MobileImage 데이터 클래스 추가

---
+++ app/src/main/java/com/survivalcoding/ifkakao/model/LinkList.kt
@@ -0,0 +1,10 @@
+package com.survivalcoding.ifkakao.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class LinkList(
+    @Json(name = "MO_IMAGE")
+    val mobileImage: List<MobileImage>
+)
