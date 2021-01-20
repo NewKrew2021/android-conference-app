c9b3439dafe9389a438a4c6cee23a9b448b3898f
finn k <finn.k@kakaocorp.com>
2021-01-20 20:35:16 +0900

    - 레포지토리 모듈 추가

---
+++ app/src/main/java/com/survivalcoding/ifkakao/di/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package com.survivalcoding.ifkakao.di
+
+import com.survivalcoding.ifkakao.data.repository.ConferenceRepository
+import org.koin.dsl.module
+
+
+val repositoryModule = module {
+    single { ConferenceRepository() }
+}
