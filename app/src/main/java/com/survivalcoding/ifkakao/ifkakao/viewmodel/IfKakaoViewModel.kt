e65852176a92cfe84fde8ea7d952f1d0f7daa913
Sanghun-Lee <paa0609@naver.com>
2021-01-20 22:06:48 +0900

    ViewModel 작성 (postValue 동작x, 좀 더 구현 필요)

--- app/src/main/java/com/survivalcoding/ifkakao/ifkakao/viewmodel/IfKakaoViewModel.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/ifkakao/viewmodel/IfKakaoViewModel.kt
@@ -1,6 +1,26 @@
 package com.survivalcoding.ifkakao.ifkakao.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.survivalcoding.ifkakao.ifkakao.data.DataModel
+import com.survivalcoding.ifkakao.ifkakao.data.DefaultDataModel
+import com.survivalcoding.ifkakao.ifkakao.model.IfKakaoItem
+import kotlin.reflect.KProperty
 
 class IfKakaoViewModel : ViewModel() {
+    private val _ifKakaoItem: MutableLiveData<IfKakaoItem> by lazy {
+        MutableLiveData<IfKakaoItem>().also {
+            loadIfKakaoItem()
+        }
+    }
+    val ifkakaoItem: LiveData<IfKakaoItem>
+        get() = _ifKakaoItem
+
+    private val ifKakaoModel: DefaultDataModel = DataModel()
+
+    private fun loadIfKakaoItem() {
+        val parsedData = ifKakaoModel.getIfKakaoItem(ifKakaoModel.getRequest())
+//        _ifKakaoItem.postValue(parsedData)
+    }
 }
