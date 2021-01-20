226482e48fcddeda6e05edef3d723e06f329cdfb
summer.dev <summer.dev@kakaocorp.com>
2021-01-20 10:35:48 +0900

    - fragment, activity 연동

--- app/src/main/java/com/survivalcoding/ifkakao/second/view/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/second/view/MainActivity.kt
@@ -2,11 +2,24 @@ package com.survivalcoding.ifkakao.second.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.survivalcoding.ifkakao.R
+import com.survivalcoding.ifkakao.databinding.SecondActivityMainBinding
+import com.survivalcoding.ifkakao.second.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: SecondActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.second_activity_main)
+        binding = SecondActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<MainFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
