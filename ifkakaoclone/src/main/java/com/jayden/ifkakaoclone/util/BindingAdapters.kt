c4aedc47f9e08d95c6a591f6895f9e69c78246d5
qufwnfahs <style_55@naver.com>
2021-01-21 17:59:46 +0900

    loadImageFromUrlWithPlaceHolder() 범용성 및 가독성 개선

--- ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/util/BindingAdapters.kt
+++ ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/util/BindingAdapters.kt
@@ -6,12 +6,19 @@ import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
 
-@BindingAdapter("imageUrl", "placeholderDrawable", "roundCornerRadius")
-fun loadImageFromUrlWithPlaceHolder(imageView: ImageView, imageUrl: String, placeholderDrawable: Drawable, roundCornerRadius: Float) {
-    imageView.load(imageUrl) {
+@BindingAdapter(
+    value = ["imageUrl", "placeholderDrawable", "roundCornerRadius"],
+    requireAll = false
+)
+fun ImageView.loadImageFromUrlWithPlaceHolder(
+    imageUrl: String,
+    placeholderDrawable: Drawable,
+    roundCornerRadius: Float,
+) {
+    load(imageUrl) {
         crossfade(true)
         placeholder(placeholderDrawable)
-        transformations(RoundedCornersTransformation(roundCornerRadius))
+        if (roundCornerRadius >= 0f) transformations(RoundedCornersTransformation(roundCornerRadius))
     }
 }
 
