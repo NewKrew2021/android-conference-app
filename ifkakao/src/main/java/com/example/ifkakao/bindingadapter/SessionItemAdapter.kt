803d63811f96c3ff69239235c10695b3ac2f5e4e
tony.mb <tony.mb@kakaocorp.com>
2021-01-21 17:20:23 +0900

    Coil에서 Glide로 교체

--- ifkakao/src/main/java/com/example/ifkakao/bindingadapter/SessionItemAdapter.kt
+++ ifkakao/src/main/java/com/example/ifkakao/bindingadapter/SessionItemAdapter.kt
@@ -4,9 +4,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
-import coil.load
-import coil.transform.RoundedCornersTransformation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.example.ifkakao.R
+
 
 @BindingAdapter("sessionTitle")
 fun setSessionTitle(textView: TextView, sessionTitle: String) {
@@ -15,9 +17,13 @@ fun setSessionTitle(textView: TextView, sessionTitle: String) {
 
 @BindingAdapter("thumbnailImageUrl")
 fun setImageFromUrl(imageView: ImageView, url: String) {
-    imageView.load(url) {
-        crossfade(true)
-        placeholder(R.drawable.temp_thumbnail)
-        transformations(listOf(RoundedCornersTransformation(5f)))
-    }
+    // CrossFade 전환 효과를 위한 factory
+    val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
+
+    Glide.with(imageView)
+        .load(url)
+        .transition(withCrossFade(factory))
+        .centerInside()
+        .placeholder(R.drawable.temp_thumbnail)
+        .into(imageView)
 }
