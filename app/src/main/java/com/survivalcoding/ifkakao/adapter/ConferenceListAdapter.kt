bd269c3160741f897bab0c85049d99fbf7863be5
tomy.tok <tomy.tok@kakaocorp.com>
2021-01-21 14:52:53 +0900

    Adapt DataBinding in app

--- app/src/main/java/com/survivalcoding/ifkakao/adapter/ConferenceListAdapter.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/adapter/ConferenceListAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.survivalcoding.ifkakao.databinding.ConferenceItemBinding
-import com.survivalcoding.ifkakao.extension.loadUrl
 import com.survivalcoding.ifkakao.holder.ConferenceListViewHolder
 import com.survivalcoding.ifkakao.model.conferenceData.Data
 
@@ -20,28 +19,7 @@ class ConferenceListAdapter :
 
     override fun onBindViewHolder(holder: ConferenceListViewHolder, position: Int) {
         val currentItem = getItem(holder.adapterPosition)
-        holder.binding.apply {
-            field.text = currentItem.field
-
-            if(currentItem.title.contains("<br>")){
-                val res = currentItem.title.split("<br>")
-                val stringBuffer = StringBuffer()
-                for(token in res){
-                    stringBuffer.append(token)
-                    stringBuffer.append("\n")
-                }
-                title.text = stringBuffer.toString()
-            }else{
-                title.text = currentItem.title
-            }
-            thumbnail.loadUrl(currentItem.linkList.pcImage[0].url)
-            runningTime.text = getRunningTime(currentItem)
-        }
+        holder.binding.item = currentItem
     }
-
-    fun getRunningTime(conferenceItem: Data) : String{
-        return conferenceItem.linkList.video[0].description
-    }
-
 
 }
