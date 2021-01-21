7b158f0f6b1c715c5bffb43bcdbc7d616c4d260b
bsgreentea1 <bsgreentea1@gmail.com>
2021-01-21 19:21:33 +0900

    확장함수 네이밍 변경

--- app/src/main/java/com/survivalcoding/ifkakao/extension/StringExtensions.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/extension/StringExtensions.kt
@@ -2,6 +2,6 @@ package com.survivalcoding.ifkakao.extension
 
 import androidx.core.text.HtmlCompat
 
-fun String.getStringExceptTags() = HtmlCompat.fromHtml(
+fun String.transformTagsToNewline() = HtmlCompat.fromHtml(
     this, HtmlCompat.FROM_HTML_MODE_LEGACY
 ).toString()
