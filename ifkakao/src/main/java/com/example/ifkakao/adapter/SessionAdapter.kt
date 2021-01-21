e2a0ff6e87e8a5bf5f22157229943a6cbda8d8a1
tony.mb <tony.mb@kakaocorp.com>
2021-01-21 18:25:40 +0900

    NestedScrollView 제거하고 RecyclerView에 Header, Footer 추가

--- ifkakao/src/main/java/com/example/ifkakao/adapter/SessionAdapter.kt
+++ ifkakao/src/main/java/com/example/ifkakao/adapter/SessionAdapter.kt
@@ -5,21 +5,64 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ifkakao.databinding.ItemSessionBinding
+import com.example.ifkakao.databinding.SessionListFooterBinding
+import com.example.ifkakao.databinding.SessionListHeaderBinding
 import com.example.ifkakao.model.jsonformat.Session
 
 class SessionAdapter :
-    ListAdapter<Session, SessionAdapter.SessionViewHolder>(SessionDiffUtilCallback()) {
+    ListAdapter<Session, RecyclerView.ViewHolder>(SessionDiffUtilCallback()) {
     private lateinit var binding: ItemSessionBinding
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SessionViewHolder {
-        binding = ItemSessionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return SessionViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_HEADER -> SessionHeaderViewHolder(
+                SessionListHeaderBinding.inflate(
+                    LayoutInflater.from(
+                        parent.context
+                    ), parent, false
+                )
+            )
+            TYPE_FOOTER -> SessionFooterViewHolder(
+                SessionListFooterBinding.inflate(
+                    LayoutInflater.from(
+                        parent.context
+                    ), parent, false
+                )
+            )
+            else -> {
+                binding =
+                    ItemSessionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                SessionViewHolder(binding)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: SessionViewHolder, position: Int) {
-        binding.session = getItem(position)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is SessionViewHolder) {
+            binding.session = getItem(position)
+        }
     }
 
-    class SessionViewHolder(private val binding: ItemSessionBinding) :
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> TYPE_HEADER
+            itemCount - 1 -> TYPE_FOOTER
+            else -> TYPE_ITEM
+        }
+    }
+
+    class SessionHeaderViewHolder(binding: SessionListHeaderBinding) :
         RecyclerView.ViewHolder(binding.root)
+
+    class SessionFooterViewHolder(binding: SessionListFooterBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    class SessionViewHolder(binding: ItemSessionBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    companion object {
+        const val TYPE_HEADER = 0
+        const val TYPE_ITEM = 1
+        const val TYPE_FOOTER = 2
+    }
 }
