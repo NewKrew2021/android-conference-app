38fa49679e2b524645449eacdc4fba2087f85f29
tony.mb <tony.mb@kakaocorp.com>
2021-01-22 15:40:33 +0900

    바인딩 어뎁터 확장함수 형태로 수정

--- ifkakao/src/main/java/com/example/ifkakao/bindingadapter/SessionItemAdapter.kt
+++ ifkakao/src/main/java/com/example/ifkakao/bindingadapter/SessionItemAdapter.kt
@@ -5,25 +5,34 @@ import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.example.ifkakao.R
 
-
 @BindingAdapter("sessionTitle")
-fun setSessionTitle(textView: TextView, sessionTitle: String) {
-    textView.text = HtmlCompat.fromHtml(sessionTitle, HtmlCompat.FROM_HTML_MODE_LEGACY)
+fun TextView.setSessionTitle(sessionTitle: String) {
+    this.text = HtmlCompat.fromHtml(sessionTitle, HtmlCompat.FROM_HTML_MODE_LEGACY)
 }
 
-@BindingAdapter("thumbnailImageUrl")
-fun setImageFromUrl(imageView: ImageView, url: String) {
+@BindingAdapter("thumbnailImageUrl", "cornerRadius", requireAll = false)
+fun ImageView.setImageFromUrl(url: String, roundingRadius: Int = 0) {
     // CrossFade 전환 효과를 위한 factory
     val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
 
-    Glide.with(imageView)
+    Glide.with(this)
         .load(url)
-        .transition(withCrossFade(factory))
-        .centerInside()
+        .transition(withCrossFade(factory)).apply {
+            // 모서리 부분 둥글게 처리
+            if (roundingRadius > 0) this.apply(
+                RequestOptions().transform(
+                    CenterInside(),
+                    RoundedCorners(roundingRadius)
+                )
+            ) else this.centerInside()
+        }
         .placeholder(R.drawable.temp_thumbnail)
-        .into(imageView)
+        .into(this)
 }
