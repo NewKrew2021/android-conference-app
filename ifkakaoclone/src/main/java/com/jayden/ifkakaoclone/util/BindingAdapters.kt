687f7c48b9e47b67c02db9cfb2d7012318ddda06
qufwnfahs <style_55@naver.com>
2021-01-23 18:21:38 +0900

    loadImageFromUrlWithPlaceHolder 기능 추가 - CircleCrop

--- ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/util/BindingAdapters.kt
+++ ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/util/BindingAdapters.kt
@@ -5,21 +5,29 @@ import android.widget.ImageView
 import android.widget.VideoView
 import androidx.databinding.BindingAdapter
 import coil.load
+import coil.transform.CircleCropTransformation
 import coil.transform.RoundedCornersTransformation
+import coil.transform.Transformation
 
 @BindingAdapter(
-    value = ["imageUrl", "placeholderDrawable", "roundCornerRadius"],
+    value = ["imageUrl", "placeholderDrawable", "roundCornerRadius", "isCircleCrop"],
     requireAll = false
 )
 fun ImageView.loadImageFromUrlWithPlaceHolder(
     imageUrl: String,
     placeholderDrawable: Drawable,
     roundCornerRadius: Float,
+    isCircleCrop: Boolean
 ) {
+    val transformationBuilder = mutableListOf<Transformation>().apply {
+        if (roundCornerRadius >= 0f) add(RoundedCornersTransformation(roundCornerRadius))
+        if (isCircleCrop) add(CircleCropTransformation())
+    }
+
     load(imageUrl) {
         crossfade(true)
         placeholder(placeholderDrawable)
-        if (roundCornerRadius >= 0f) transformations(RoundedCornersTransformation(roundCornerRadius))
+        transformations(transformationBuilder)
     }
 }
 
