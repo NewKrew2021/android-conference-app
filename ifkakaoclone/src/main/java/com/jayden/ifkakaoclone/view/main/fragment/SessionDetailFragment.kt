9a86d1f23cf1118e77625ce3a7ee512e856eb06a
qufwnfahs <style_55@naver.com>
2021-01-23 10:27:01 +0900

    SessionDetailFragment 생성

---
+++ ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/view/main/fragment/SessionDetailFragment.kt
@@ -0,0 +1,36 @@
+package com.jayden.ifkakaoclone.view.main.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.jayden.ifkakaoclone.data.viewmodel.SessionViewModel
+import com.jayden.ifkakaoclone.databinding.FragmentSessionDetailBinding
+
+class SessionDetailFragment : Fragment() {
+    private var _binding: FragmentSessionDetailBinding? = null
+    private val binding
+        get() =_binding!!
+
+    private val activityViewModel: SessionViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSessionDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
