c40fe0dd5fe92f7668230514a509703043bf3cda
Sanghun-Lee <paa0609@naver.com>
2021-01-24 03:14:32 +0900

    Spinner Adapter, VideoView 적용

--- app/src/main/java/com/survivalcoding/ifkakao/ifkakao/view/main/IfKakaoFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/ifkakao/view/main/IfKakaoFragment.kt
@@ -1,20 +1,25 @@
 package com.survivalcoding.ifkakao.ifkakao.view.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.core.net.toUri
+import androidx.core.view.MenuCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.survivalcoding.ifkakao.R
+import com.survivalcoding.ifkakao.databinding.FragmentCoordinatorBinding
 import com.survivalcoding.ifkakao.databinding.FragmentIfKakaoBinding
 import com.survivalcoding.ifkakao.ifkakao.model.Data
 import com.survivalcoding.ifkakao.ifkakao.view.main.adapter.IfKakaoAdapter
 import com.survivalcoding.ifkakao.ifkakao.viewmodel.IfKakaoViewModel
 
 class IfKakaoFragment() : Fragment() {
-    private var _binding: FragmentIfKakaoBinding? = null
+    private var _binding: FragmentCoordinatorBinding? = null
     private val binding get() = _binding!!
 
     val adapter = IfKakaoAdapter()
@@ -24,7 +29,7 @@ class IfKakaoFragment() : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentIfKakaoBinding.inflate(inflater, container, false)
+        _binding = FragmentCoordinatorBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -35,9 +40,32 @@ class IfKakaoFragment() : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireActivity().title = "If Kakao 2020"
         binding.apply {
             ifKakaoListView.adapter = adapter
+
+            // spinner
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.day_select,
+                R.layout.item_spinner
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                daySelectSpinner.adapter = adapter
+            }
+
+            // filter button
+            filterButton.setOnClickListener {
+                Toast.makeText(
+                    requireContext(),
+                    "filter button clicked - IfKakaoFragment",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            // video view
+            teaserPlay(vodTeaser)
         }
 
         // ViewModel 가져오기.
@@ -48,6 +76,14 @@ class IfKakaoFragment() : Fragment() {
         })
 
         model.loadIfKakaoItem()
+    }
+
+    fun teaserPlay(videoView: VideoView) {
+        videoView.setVideoURI("https://t1.kakaocdn.net/service_if_kakao_prod/videos/mo/vod_teaser.mp4".toUri())
+        videoView.setOnPreparedListener {
+            videoView.start()
+            it.isLooping = true;
+        }
     }
 
     fun updateUi(list: List<Data>) {
