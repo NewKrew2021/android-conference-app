872c49941d993fe70ae632880b9f28d6c18085d9
noah.choung <noah.choung@kakaocorp.com>
2021-01-24 11:18:27 +0900

    - 툴바 글씨 누르면 홈으로 돌아가기 ,, 툴바 액티비티에만 하나로 구현

--- app/src/main/java/com/survivalcoding/ifkakao/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.survivalcoding.ifkakao.databinding.ActivityMainBinding
 import com.survivalcoding.ifkakao.factory.MyFragmentFactory
 
@@ -26,7 +27,17 @@ class MainActivity : AppCompatActivity() {
                 add<HighlightFragment>(R.id.fragment_container_view)
             }
         }
-    }
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setTitle("")
 
+        setSupportActionBar(binding.toolbar)
+        binding.textView.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<HighlightFragment>(R.id.fragment_container_view)
+            }
+        }
+
+    }
 
 }
--- app/src/main/java/com/survivalcoding/ifkakao/MainFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/MainFragment.kt
@@ -88,6 +88,7 @@ class MainFragment : Fragment() {
             }
         }
 
+
         return view
     }
 
