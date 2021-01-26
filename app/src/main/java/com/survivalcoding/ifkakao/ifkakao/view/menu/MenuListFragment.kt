12136a7ef6c6cffcb2825883c107e755ff621207
dale.band <dale.band@kakaocorp.com>
2021-01-26 18:40:21 +0900

    메뉴화면 fragment생성, Session선택시 메인화면으로 이동

---
+++ app/src/main/java/com/survivalcoding/ifkakao/ifkakao/view/menu/MenuListFragment.kt
@@ -0,0 +1,42 @@
+package com.survivalcoding.ifkakao.ifkakao.view.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.survivalcoding.ifkakao.R
+import com.survivalcoding.ifkakao.databinding.FragmentMenuListBinding
+import com.survivalcoding.ifkakao.ifkakao.view.main.IfKakaoFragment
+
+class MenuListFragment() : Fragment() {
+    private var _binding: FragmentMenuListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMenuListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.menuSessionText.setOnClickListener {
+            parentFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<IfKakaoFragment>(R.id.if_kakao_fragment_container_view)
+            }
+        }
+    }
+}
