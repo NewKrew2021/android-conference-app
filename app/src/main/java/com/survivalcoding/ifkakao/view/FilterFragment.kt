63a8c4e36e1fbc8171130832b8182577c2c825c0
tomy.tok <tomy.tok@kakaocorp.com>
2021-01-26 21:37:26 +0900

    1. Two way dataBinding 2. Make Filter Base Layout

--- app/src/main/java/com/survivalcoding/ifkakao/view/FilterFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/view/FilterFragment.kt
@@ -5,13 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.survivalcoding.ifkakao.databinding.FragmentFilterBinding
+import com.survivalcoding.ifkakao.viewmodel.FilterViewModel
 
 
 class FilterFragment : Fragment() {
     private var _bindng: FragmentFilterBinding? = null
     private val binding get() = _bindng!!
-
+    val viewModel: FilterViewModel by activityViewModels()
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -29,6 +31,7 @@ class FilterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.viewModels = viewModel
+        binding.lifecycleOwner = requireActivity()
     }
 }
