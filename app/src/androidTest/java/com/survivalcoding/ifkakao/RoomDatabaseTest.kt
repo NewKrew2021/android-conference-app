74e056dfcd418a17f34b8f2b8c303d90c8e77894
summer.dev <summer.dev@kakaocorp.com>
2021-01-27 17:05:15 +0900

    - RoomDB test 코드 작성

---
+++ app/src/androidTest/java/com/survivalcoding/ifkakao/RoomDatabaseTest.kt
@@ -0,0 +1,56 @@
+package com.survivalcoding.ifkakao
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.survivalcoding.ifkakao.second.model.favorite.database.AppDatabase
+import com.survivalcoding.ifkakao.second.model.favorite.database.Favorite
+import com.survivalcoding.ifkakao.second.model.favorite.database.FavoriteDao
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class RoomDatabaseTest {
+    private lateinit var favoriteDao: FavoriteDao
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, AppDatabase::class.java
+        ).build()
+        favoriteDao = db.favoriteDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun wrightAndReadList() {
+        val data = listOf(
+            Favorite(0, false),
+            Favorite(1, true),
+            Favorite(2, false),
+            Favorite(1, false)
+        )
+        data.forEach { favoriteDao.insert(it) }
+        var datas = favoriteDao.getAll()
+        assertEquals(datas.size, 3)
+        assertEquals(datas[0].isFavorite, false)
+        assertEquals(datas[1].isFavorite, false)
+        assertEquals(datas[2].isFavorite, false)
+        data.forEach { favoriteDao.delete(it) }
+        datas = favoriteDao.getAll()
+        assertEquals(datas.size, 0)
+    }
+}
