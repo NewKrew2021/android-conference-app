8a32cc842e3e39f9e783d3908fc30a0471ab6d49
dale.band <dale.band@kakaocorp.com>
2021-01-28 15:36:27 +0900

    테스트 코드 수정

--- app/src/androidTest/java/com/survivalcoding/ifkakao/RoomDatabaseTest.kt
+++ app/src/androidTest/java/com/survivalcoding/ifkakao/RoomDatabaseTest.kt
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith
 import com.survivalcoding.ifkakao.ifkakao.database.FavoriteDao
 import com.survivalcoding.ifkakao.ifkakao.database.FavoriteTable
 import com.survivalcoding.ifkakao.ifkakao.database.InAppDataBase
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.After
 import org.junit.Assert.assertThat
@@ -22,34 +23,34 @@ import kotlin.jvm.Throws
 //  > A failure occurred while executing org.jetbrains.kotlin.gradle.internal.KaptExecution
 //   > java.lang.reflect.InvocationTargetException (no error message)
 
-//@RunWith(AndroidJUnit4::class)
-//class RoomDatabaseTest() {
-//    private lateinit var favoriteDao: FavoriteDao
-//    private lateinit var db: InAppDataBase
-//
-//    @Before
-//    fun createDb() {
-//        val context = ApplicationProvider.getApplicationContext<Context>()
-//        db = Room.inMemoryDatabaseBuilder(
-//            context, InAppDataBase::class.java
-//        ).allowMainThreadQueries().build()
-//        favoriteDao = db.favoriteDao()
-//    }
-//
-//    @After
-//    @Throws(IOException::class)
-//    fun closeDb() {
-//        db.close()
-//    }
-//
-//    @Test
-//    @Throws(Exception::class)
-//    suspend fun InsertDataAndGetData() {
-//        val idx = FavoriteTable(29, 0)
-//        favoriteDao.insertIdx(idx)
-//
-//        val getData = favoriteDao.getAll()
-//        assertThat(getData[0], equalTo(idx))
-//    }
-//
-//}
+@RunWith(AndroidJUnit4::class)
+class RoomDatabaseTest() {
+    private lateinit var favoriteDao: FavoriteDao
+    private lateinit var db: InAppDataBase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, InAppDataBase::class.java
+        ).allowMainThreadQueries().build()
+        favoriteDao = db.favoriteDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertDataAndGetData() {
+        val idx = FavoriteTable(29, 0)
+        favoriteDao.insertIdx(idx)
+
+        val getData = favoriteDao.getAll()
+        assertThat(getData[0], equalTo(idx))
+    }
+
+}
