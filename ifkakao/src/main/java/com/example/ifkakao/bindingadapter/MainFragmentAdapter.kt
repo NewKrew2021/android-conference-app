a3fb3610b625e272e4f0650214a58e52b3ff65a0
tony.mb <tony.mb@kakaocorp.com>
2021-01-29 21:27:51 +0900

    메인 프래그먼트에 필터링 개수 표시

--- ifkakao/src/main/java/com/example/ifkakao/bindingadapter/MainFragmentAdapter.kt
+++ ifkakao/src/main/java/com/example/ifkakao/bindingadapter/MainFragmentAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.ifkakao.bindingadapter
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.updateLayoutParams
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ifkakao.adapter.SessionAdapter
@@ -9,4 +12,11 @@ import com.example.ifkakao.model.jsonformat.Session
 fun RecyclerView.setSessionList(sessionList: List<Session>) {
     val adapter = (this.adapter as SessionAdapter)
     adapter.submitList(sessionList)
+}
+
+@BindingAdapter("layout_width")
+fun View.setLayoutWidth(dimen: Float) {
+    updateLayoutParams<ViewGroup.MarginLayoutParams> {
+        width = dimen.toInt()
+    }
 }
