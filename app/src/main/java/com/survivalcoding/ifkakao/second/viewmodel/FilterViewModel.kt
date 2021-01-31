61cde41774d0ad5f0282c1a463da4aa96e9c4945
summer.dev <summer.dev@kakaocorp.com>
2021-01-31 21:42:17 +0900

    - Filter dataclass 생성 및 parcelize변환

--- app/src/main/java/com/survivalcoding/ifkakao/second/viewmodel/FilterViewModel.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/second/viewmodel/FilterViewModel.kt
@@ -3,38 +3,40 @@ package com.survivalcoding.ifkakao.second.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.survivalcoding.ifkakao.second.model.filter.Filter
 import com.survivalcoding.ifkakao.second.model.filter.FilterType
 import com.survivalcoding.ifkakao.second.util.find
 
 class FilterViewModel : ViewModel() {
 
-    private val _filters = MutableLiveData<Map<FilterType, String>>(mapOf())
-    val filters: LiveData<Map<FilterType, String>> get() = _filters
+
+    private val _filters = MutableLiveData<Filter>(Filter(mapOf()))
+    val filters: LiveData<Filter> get() = _filters
 
 
     fun addFilter(filterType: FilterType, name: String) {
         _filters.value?.let {
-            _filters.value = it.toMutableMap().apply {
+            _filters.value = Filter(it.data.toMutableMap().apply {
                 put(filterType, name)
-            }
+            })
         }
     }
 
     fun removeFilter(filterType: FilterType) {
         _filters.value?.let {
-            _filters.value = it.toMutableMap().apply {
+            _filters.value = Filter(it.data.toMutableMap().apply {
                 remove(filterType)
-            }
+            })
         }
     }
 
 
     fun resetFilter() {
-        _filters.value = mapOf()
+        _filters.value = Filter(mapOf())
     }
 
     fun isSelectedFilter(filterType: FilterType, name: String): Boolean =
-        _filters.value?.find(filterType, name) ?: false
+        _filters.value?.data?.find(filterType, name) ?: false
 
 
 //    fun submitFilter() {
