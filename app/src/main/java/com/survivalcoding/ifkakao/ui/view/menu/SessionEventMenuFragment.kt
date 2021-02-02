1b35c79992cbd65e2eaea86799d9c57f258bd9b7
finn k <finn.k@kakaocorp.com>
2021-02-02 18:13:57 +0900

    SessionEventMenuFragment.kt 이벤트처리 xml에 바인딩

--- app/src/main/java/com/survivalcoding/ifkakao/ui/view/menu/SessionEventMenuFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/ui/view/menu/SessionEventMenuFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.View
 import com.survivalcoding.ifkakao.R
 import com.survivalcoding.ifkakao.databinding.FragmentSessionEventMenuBinding
-import com.survivalcoding.ifkakao.extension.navigate
-import com.survivalcoding.ifkakao.extension.popBackStack
 import com.survivalcoding.ifkakao.ui.base.BaseFragment
 import com.survivalcoding.ifkakao.ui.viewmodel.SessionEventMenuViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -20,22 +18,7 @@ class SessionEventMenuFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setOnClick()
+        binding.fragment = this
     }
 
-    private fun setOnClick() {
-        binding.run {
-            ivCloseSessionEventMenu.setOnClickListener {
-                popBackStack()
-            }
-
-            tvSessionSessionEvent.setOnClickListener {
-                navigate(R.id.fragment_session)
-            }
-
-            tvEventSessionEvent.setOnClickListener {
-                navigate(R.id.fragment_event)
-            }
-        }
-    }
 }
