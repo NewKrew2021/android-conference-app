ce2452f245290aff7def2d4efe866528b6be0540
qufwnfahs <style_55@naver.com>
2021-02-03 16:01:50 +0900

    shouldShowRequestPermissionRationale() 분기 정리 - ActivityCompat 메소드 사용

--- ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/view/permission/PermissionActivity.kt
+++ ifkakaoclone/src/main/java/com/jayden/ifkakaoclone/view/permission/PermissionActivity.kt
@@ -2,12 +2,12 @@ package com.jayden.ifkakaoclone.view.permission
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.jayden.ifkakaoclone.R
 
@@ -42,33 +42,25 @@ class PermissionActivity : AppCompatActivity() {
     }
 
     private fun checkRuntimePermission() {
-        // shouldShowRequestPermissionRationale 에 대한 분기
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            when {
-                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                        == PackageManager.PERMISSION_GRANTED -> {
-                    Log.d(javaClass.simpleName, "사용자가 권한을 동의하였습니다.")
-                    fakeFunctionWithCamera()
-                }
-                // Deny 할 시 다음 실행부터 이 곳으로 분기된다.
-                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
-                    // 왜 이 권한이 필요한지 설명하고, "취소"나 "괜찮습니다" 같은 버튼을 포함해서 사용자가 권한 승인 없이 앱을 사용하도록 해야 하는 곳
-                    showPermissionRequestExplanation(
-                        Manifest.permission.CAMERA,
-                        "무슨 기능 때문에 카메라 권한이 필요합니다."
-                    ) { requestPermissionLauncher.launch(Manifest.permission.CAMERA) }
-                }
-                else -> {
-                    // ActivityRequestLauncher.launch() 시 해당 권한 승인 dialog 가 보여진다.
-                    requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-                }
-            }
-        } else {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED) {
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED -> {
                 Log.d(javaClass.simpleName, "사용자가 권한을 동의하였습니다.")
                 fakeFunctionWithCamera()
-            } else {
+            }
+            // Deny 할 시 다음 실행부터 이 곳으로 분기된다.
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                // 왜 이 권한이 필요한지 설명하고, "취소"나 "괜찮습니다" 같은 버튼을 포함해서 사용자가 권한 승인 없이 앱을 사용하도록 해야 하는 곳
+                showPermissionRequestExplanation(
+                    Manifest.permission.CAMERA,
+                    "무슨 기능 때문에 카메라 권한이 필요합니다."
+                ) { requestPermissionLauncher.launch(Manifest.permission.CAMERA) }
+            }
+            else -> {
+                // ActivityRequestLauncher.launch() 시 해당 권한 승인 dialog 가 보여진다.
                 requestPermissionLauncher.launch(Manifest.permission.CAMERA)
             }
         }
